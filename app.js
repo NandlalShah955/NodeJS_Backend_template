7f614b7e979891d91a650f3b73a8837139ec061f
“Nandlal_shah” <shahnandlal49@gmail.com>
2024-04-22 14:21:16 +0530

    Database added successfully

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectdb from './config/connectdb.js';
 dotenv.config(); //Note You will not be needed Dotenv package if you are using latest 20 version of Nodejs;
+
 const app = express();
 const port=process.env.port;
-app.use(cors());
+const DATABASE_URL = process.env.DATABASE_URL;
 
-app.get('/',(req,res)=>{
+// For using Cors 
+app.use(cors());
+// For connecting Database 
+connectdb(DATABASE_URL);
+// Basic route 
+app.get('/ping',(req,res)=>{
     console.log("heelo");
+    res.status(200).send("Hellow there");
+
 })
 
 app.listen(port,()=>{
